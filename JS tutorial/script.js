72a0b05a7ac3588c495b2d22f56ba75fc7a64268
EvilCircusClown <cedrickdalemanjares@gmail.com>
2024-12-19 05:27:05 +0800

    v9

--- JS tutorial/script.js
+++
@@ -1,7 +0,0 @@
-let Science = 98
-let Mathematics = 95
-let History = 81
-let English = 96
-let Arts = 85
-
-document.getElementById("ee").innerHTML = ((Science + Mathematics + History + English + Arts)/5)
